0d61924713e73bec44895d1df3992f44d9ea1d45
tjddhks6814 <tjddhks6814@naver.com>
2023-01-08 15:08:34 +0900

    useProps commit

---
+++ src/components/TSpropsTest.tsx
@@ -0,0 +1,9 @@
+import TestMeno from "./TSpropsTestChildren";
+
+const TestMenoParent = () => {
+    return(
+    <TestMeno name={"서성완"} age={28} school={"대졸"}/>
+    );
+}
+
+export default TestMenoParent
